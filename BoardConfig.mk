65c0533c7926f621f49c7bde7f455852ddffc7de
Sandeep Neerudu <sneerudu@codeaurora.org>
2019-10-14 15:04:19 +0530

    lahaina : remove duplicate copy of USE_SENSOR_HAL_VER
    USE_SENSOR_HAL_VER is already defined part of component
    
    specific board config file
    
    Change-Id: I7d0161abaeb9c0427d0ad49a144eacde14ecbbc4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,9 +196,6 @@ ADD_RADIO_FILES := true
 # Enable sensor multi HAL
 USE_SENSOR_MULTI_HAL := true
 
-# Enable sensor Version V_2
-USE_SENSOR_HAL_VER := 2.0
-
 #flag for qspm compilation
 TARGET_USES_QSPM := true
 
