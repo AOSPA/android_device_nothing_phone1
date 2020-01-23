a6ebffab9a12734804960f99e9ea3f24b3c6cba2
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2020-01-23 14:34:23 -0800

    Add charger_fstab.qti for Lahaina
    ADSP needs to be bought out of reset in charger mode to support
    battery charging. Since ADSP firmware is part of modem partition,
    add charger_fstab.qti that can be used to mount modem partition
    in charger mode.
    
    Change-Id: I0545da94d427fd9ac38a47cd294ed12a41075a39

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -53,6 +53,14 @@ endif
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := charger_fstab.qti
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := charger_fstab.qti
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
 include device/qcom/vendor-common/MergeConfig.mk
 
 #----------------------------------------------------------------------
