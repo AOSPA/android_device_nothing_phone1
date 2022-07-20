7bf02627391c042561a02e00a207af58a6739a1a
Jake Weinstein <jake@aospa.co>
2022-07-20 14:07:50 -0500

    phone1: Drop BOARD_HAVE_BLUETOOTH := false
    This device does have BT.

--- device.mk
+++ device.mk
@@ -37,7 +37,6 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAM
 
 $(call inherit-product, build/make/target/product/gsi_keys.mk)
 
-BOARD_HAVE_BLUETOOTH := false
 BOARD_HAVE_QCOM_FM := false
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
