ed531420004ff9e56cc1bc8ddb00214198294c2f
Jake Weinstein <jake@aospa.co>
2022-07-19 20:41:00 -0500

    phone1: Remove privapp enforcement
    We do this globally

--- device.mk
+++ device.mk
@@ -55,9 +55,6 @@ $(call inherit-product, build/make/target/product/gsi_keys.mk)
 BOARD_HAVE_BLUETOOTH := false
 BOARD_HAVE_QCOM_FM := false
 
-# privapp-permissions whitelisting (To Fix CTS :privappPermissionsMustBeEnforced)
-PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
-
 TARGET_DEFINES_DALVIK_HEAP := true
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
