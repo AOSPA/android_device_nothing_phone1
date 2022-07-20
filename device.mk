e1882069f5f03a3da45c5ce2adce65b0278d3b72
Jake Weinstein <jake@aospa.co>
2022-07-20 10:13:53 -0500

    phone1: Remove TARGET_DEFINES_DALVIK_HEAP
    This is used in device/qcom/vendor-common to inherit the
    1GB RAM configuration if another dalvik Makefile has not
    already been inherited.  We don't inherit vendor-common.

--- device.mk
+++ device.mk
@@ -40,7 +40,6 @@ $(call inherit-product, build/make/target/product/gsi_keys.mk)
 BOARD_HAVE_BLUETOOTH := false
 BOARD_HAVE_QCOM_FM := false
 
-TARGET_DEFINES_DALVIK_HEAP := true
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 ###########
