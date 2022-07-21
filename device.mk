004f4da9c814a83c42d24266c4b605863b775fa5
Jake Weinstein <jake@aospa.co>
2022-07-20 22:17:29 -0500

    phone1: Add otapreopt_script to A/B postinstall config

--- device.mk
+++ device.mk
@@ -90,6 +90,12 @@ PRODUCT_PACKAGES += qrtr-lookup
 PRODUCT_PACKAGES += libqrtr
 
 AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
     FILESYSTEM_TYPE_vendor=ext4 \
