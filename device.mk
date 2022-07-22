1e72b27b0522a1b6db21ec94a90a93b7fca06bfe
Jake Weinstein <jake@aospa.co>
2022-07-23 06:25:18 +0900

    [NFC] phone1: Reorganize device makefile

--- device.mk
+++ device.mk
@@ -1,82 +1,15 @@
-TARGET_BOARD_PLATFORM := lahaina
-TARGET_BOOTLOADER_BOARD_NAME := lahaina
-
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
-# Default Android A/B configuration
+# A/B
 ENABLE_AB ?= true
-
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-BOARD_SHIPPING_API_LEVEL := 30
-BOARD_API_LEVEL := 30
-
-# Enable AVB 2.0
-BOARD_AVB_ENABLE := true
-
-#Suppot to compile recovery without msm headers
-TARGET_HAS_GENERIC_KERNEL_HEADERS := true
-
-SHIPPING_API_LEVEL := 30
-PRODUCT_SHIPPING_API_LEVEL := 30
-
-#####Dynamic partition Handling
-###
-#### Turning this flag to TRUE will enable dynamic partition/super image creation.
-PRODUCT_BUILD_ODM_IMAGE := true
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_PACKAGES += fastbootd
-# Add default implementation of fastboot HAL.
-PRODUCT_PACKAGES += android.hardware.fastboot@1.0-impl-mock
-
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
-
-$(call inherit-product, build/make/target/product/gsi_keys.mk)
-
-BOARD_HAVE_QCOM_FM := false
-
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
-###########
-
-# RRO configuration
-TARGET_USES_RRO := true
-
-TARGET_USES_QSSI := true
-
-#Default vendor image configuration
-ENABLE_VENDOR_IMAGE := true
-
-# default is nosdcard, S/W button enabled in resource
-PRODUCT_CHARACTERISTICS := nosdcard
-
-BOARD_FRP_PARTITION_NAME := frp
-
-# lights hal
-PRODUCT_PACKAGES += lights.qcom
-
-PRODUCT_PACKAGES += fs_config_files
-PRODUCT_PACKAGES += gpio-keys.kl
-
-# A/B related packages
-PRODUCT_PACKAGES += update_engine \
+PRODUCT_PACKAGES += \
+    update_engine \
     update_engine_client \
     update_verifier \
     android.hardware.boot@1.1-impl-qti \
     android.hardware.boot@1.1-impl-qti.recovery \
     android.hardware.boot@1.1-service
-
-PRODUCT_PACKAGES += \
-  update_engine_sideload
-
-# QRTR related packages
-PRODUCT_PACKAGES += qrtr-ns
-PRODUCT_PACKAGES += qrtr-lookup
-PRODUCT_PACKAGES += libqrtr
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -90,43 +23,117 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
 
-# Camera configuration file. Shared by passthrough/binderized camera HAL
-PRODUCT_PACKAGES += camera.device@3.2-impl
-PRODUCT_PACKAGES += camera.device@1.0-impl
-PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
-# Enable binderized camera HAL
-PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service_64
+# API
+BOARD_SHIPPING_API_LEVEL := 30
+BOARD_API_LEVEL := 30
+SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Architecture
+TARGET_BOARD_PLATFORM := lahaina
+TARGET_BOOTLOADER_BOARD_NAME := lahaina
+
+# Audio
+USE_LIB_PROCESS_GROUP := true
+
+# AVB
+BOARD_AVB_ENABLE := true
+
+# Build
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.device@3.2-impl \
+    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
+# Charger
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/init/charger_fw_fstab.qti:$(TARGET_COPY_OUT_VENDOR)/etc/charger_fw_fstab.qti
+
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Emulated Storage
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Filesystem
+PRODUCT_PACKAGES += \
+    fs_config_files
+
+# Fingerprint
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+# FM
+BOARD_HAVE_QCOM_FM := false
+
+# Generic Kernel Headers
+TARGET_HAS_GENERIC_KERNEL_HEADERS := true
+
+# Kernel Modules
+KERNEL_MODULES_INSTALL := dlkm
+KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_NAME)/$(KERNEL_MODULES_INSTALL)/lib/modules
+
+# Key Layout
+PRODUCT_PACKAGES += \
+    gpio-keys.kl
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.qti \
+    lights.qcom
+
+# Manifests
 DEVICE_MANIFEST_FILE += \
     device/nothing/phone1/manifest_yupik.xml \
     device/nothing/phone1/manifest_phone1.xml
 
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
 
-# Kernel modules install path
-KERNEL_MODULES_INSTALL := dlkm
-KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_NAME)/$(KERNEL_MODULES_INSTALL)/lib/modules
+# Partitions - Dynamic
+PRODUCT_BUILD_ODM_IMAGE := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-USE_LIB_PROCESS_GROUP := true
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
-#Enable full treble flag
-PRODUCT_FULL_TREBLE_OVERRIDE := true
-PRODUCT_VENDOR_MOVE_ENABLED := true
-PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
 
-TARGET_MOUNT_POINTS_SYMLINKS := false
+$(call inherit-product, build/make/target/product/gsi_keys.mk)
 
-# Fingerprint feature
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
+# Partitions - FRP
+BOARD_FRP_PARTITION_NAME := frp
 
-#Charger
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/init/charger_fw_fstab.qti:$(TARGET_COPY_OUT_VENDOR)/etc/charger_fw_fstab.qti
+# Partitions - Vendor
+ENABLE_VENDOR_IMAGE := true
+
+# QRTR
+PRODUCT_PACKAGES += \
+    qrtr-ns \
+    qrtr-lookup \
+    libqrtr
+
+# QSSI
+TARGET_USES_QSSI := true
 
 # QTI Components
 TARGET_COMMON_QTI_COMPONENTS := all
 
-PRODUCT_PACKAGES += android.hardware.lights-service.qti
+# RRO
+TARGET_USES_RRO := true
+
+# Storage
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Treble
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+PRODUCT_VENDOR_MOVE_ENABLED := true
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+TARGET_MOUNT_POINTS_SYMLINKS := false
 
 # Proprietary Vendor
 $(call inherit-product, vendor/nothing/phone1/phone1-vendor.mk)
