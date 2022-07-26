0a0451bdb0c4885456d6c44af04aa0bf91b1592c
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2022-07-26 15:31:57 +0200

    phone1: overlays: Move volume panel to the left

--- device.mk
+++ device.mk
@@ -126,7 +126,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
-    Phone1Frameworks
+    Phone1Frameworks \
+    Phone1SystemUIAOSPA
 
 # Partitions - Dynamic
 PRODUCT_BUILD_ODM_IMAGE := true
