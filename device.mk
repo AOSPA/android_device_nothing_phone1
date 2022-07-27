9d14a68e91ed5292011b771baaa433c96b5fffd8
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2022-07-27 13:03:48 +0200

    phone1: Build custom sensors multihal

--- device.mk
+++ device.mk
@@ -231,7 +231,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.phone1-multihal \
     android.hardware.sensors@2.0-ScopedWakelock \
     libsensorndkbridge
 
