39a542f2b6d4d9d5b345d39339f76c3ed8aecf1e
Jake Weinstein <jake@aospa.co>
2022-08-02 18:32:59 +0900

    phone1: Use alternate bootloader unlock property in libgf_hal.so
    libgf_hal.so checks for ro.boot.flash.locked. If it does
    not match the locked state, fingerprint unlock fails.
    We spoof this property for SafetyNet, so check
    for ro.fingerprints.lock instead.

--- extract-files.sh
+++ extract-files.sh
@@ -54,6 +54,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libgf_hal.so)
+            sed -i "s/ro.boot.flash.locked/ro.fingerprints.lock/g" "${2}"
+            ;;
+     esac
+}
+
 # Initialize the helper.
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
