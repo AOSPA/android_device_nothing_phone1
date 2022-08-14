1318886da2afa9c1c12d05f4a792e63b02984c07
Jake Weinstein <jake@aospa.co>
2022-08-15 00:51:09 +0900

    phone1: Remove libstdc++.vendor
    This is not used by our vendor.

--- device.mk
+++ device.mk
@@ -62,7 +62,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
-    libstdc++.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Dalvik
