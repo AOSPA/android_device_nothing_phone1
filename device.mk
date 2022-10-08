f25ed3c6647f168a05536cbc8ae9eb2c2ec6b3d1
Jake Weinstein <jake@aospa.co>
2022-10-09 03:55:37 +0900

    phone1: Disable power saving features on Glyph application
    This causes the light triggers to be interrupted
    on suspend.

--- device.mk
+++ device.mk
@@ -234,6 +234,10 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
+# Whitelist
+PRODUCT_COPY_FILES += \
+    device/nothing/phone1/phone1_whitelist.xml:system/etc/sysconfig/phone1_whitelist.xml
+
 # WLAN
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
