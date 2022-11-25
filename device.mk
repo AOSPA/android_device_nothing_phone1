2bf597b1a353a5c817f49568b4f3495a00039676
LuK1337 <priv.luk@gmail.com>
2022-11-25 15:46:37 +0100

    phone1: Use libcamera_metadata_shim for unlocking cameras
    Change-Id: I985c6e317b36d8cdc6b41a9f9f625b293a8987eb

--- device.mk
+++ device.mk
@@ -53,6 +53,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
+    libcamera_metadata_shim \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Dalvik
