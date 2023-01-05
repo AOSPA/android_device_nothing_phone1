3c87e2322b83ee1eb0de0bd84d126962a8385a3c
Anthony Adamo <quic_aadamo@quicinc.com>
2023-01-05 14:55:38 -0800

    phone1: Add aidl bootcontrolhal to PRODUCT_PACKAGES
    add in the aidl bootcontrol hal server implementation to
    the PRODUCT PACKAGES
    
    Change-Id: I481e337b7d92d4a4d797a1e5dee695b39159f987

--- device.mk
+++ device.mk
@@ -4,9 +4,8 @@ DEVICE_PATH := device/nothing/phone1
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service \
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery \
     checkpoint_gc \
     otapreopt_script \
     update_engine \
