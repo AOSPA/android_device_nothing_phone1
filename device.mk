d06fff81fd0185855d2376d7c71d2f191a5a7cdc
Jake Weinstein <jake@aospa.co>
2023-05-09 19:29:12 -0500

    phone1: Import mixer paths from stock
    Also remove IDP configs since phone1 is QRD.
    
    Change-Id: I05f1c18b8eab582a6bdecfec14aeeea831ed16d0

--- device.mk
+++ device.mk
@@ -41,6 +41,10 @@ PRODUCT_PACKAGES += \
 # AVB
 BOARD_AVB_ENABLE := true
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/mixer_paths_yupikqrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/mixer_paths_yupikqrd.xml
+
 # Camera
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/public.libraries.system_ext.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries.txt \
