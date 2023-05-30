132b7ca3a34e3b5a9f5232d56f5e9db7dc14bfde
Omkar Chandorkar <gotenksIN@aospa.co>
2023-05-30 14:51:53 +0900

    phone1: Switch to charging component
    Change-Id: If61fc82e86510e9b29a0800346230c0a1932b529
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -99,11 +99,6 @@ PRODUCT_PACKAGES += \
 # Generic Kernel Headers
 TARGET_HAS_GENERIC_KERNEL_HEADERS := true
 
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
-    android.hardware.health@2.1-service
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.memory.block@1.0.vendor
@@ -190,6 +185,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     audio \
     av \
     bt \
+    charging \
     display \
     gps \
     init \
