1f45adf82ed9ddd228db047abbf916f63fea63f4
cjh1249131356 <cjh1249131356@gmail.com>
2023-11-22 12:18:12 +0800

    phone1: Add NtFeaturesUtils class
    Used by Hotword Enrollment.
    
    Change-Id: I7d17b642a72d2a02e4af70f7527b6d3b5906496a
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -155,6 +155,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
 
+# Nothing-fwk
+PRODUCT_PACKAGES += \
+    nothing-fwk
+
+PRODUCT_BOOT_JARS += \
+    nothing-fwk
+
 # Overlays
 PRODUCT_PACKAGES += \
     Phone1CarrierConfig \
