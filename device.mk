8e3a7002bff20b47959c9eea1ebc9439661ecbc2
Jake Weinstein <jake@aospa.co>
2024-02-16 01:52:11 -0600

    phone1: Move IRQ configuration to device/qcom/common
    Change-Id: Ia2d4dc56e7556571e53b6b653a1974d70a867456

--- device.mk
+++ device.mk
@@ -188,10 +188,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += $(DEVICE_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
 
-# Perf
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # Powershare
 PRODUCT_PACKAGES += \
     vendor.aospa.powershare-service
