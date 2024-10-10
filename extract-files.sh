d8ae8df6cb08e8d90e47f1df2ed900224bf2b6c4
Omkar Chandorkar <gotenksIN@aospa.co>
2024-10-10 14:56:30 +0900

    phone1: Fix ELF check failures
    Change-Id: Ie11b68f1cdcd253162029655a5500112cb2e272e
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- extract-files.sh
+++ extract-files.sh
@@ -58,7 +58,7 @@ function blob_fixup() {
     case "${1}" in
         vendor/bin/hw/vendor.qti.hardware.vibrator.service | vendor/lib64/vendor.qti.hardware.vibrator.impl.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "android.hardware.vibrator-V1-ndk_platform.so" "android.hardware.vibrator-V1-ndk.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.hardware.vibrator-V1-ndk_platform.so" "android.hardware.vibrator-V2-ndk.so" "${2}"
             ;;
         vendor/etc/media_codecs.xml|vendor/etc/media_codecs_yupik_v0.xml|vendor/etc/media_codecs_yupik_v1.xml)
             [ "$2" = "" ] && return 0
