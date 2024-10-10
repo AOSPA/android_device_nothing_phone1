bf8ef843190d38bf73ce883d8de19d83879bc019
Omkar Chandorkar <gotenksIN@aospa.co>
2024-10-10 14:56:30 +0900

    phone1: Fix ELF check failures
    Change-Id: Ie11b68f1cdcd253162029655a5500112cb2e272e
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,6 +24,57 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+        "hardware/qcom/display",
+        "hardware/qcom/display/gralloc",
+        "hardware/qcom/display/libdebug",
+        "vendor/qcom/common/media-5.4",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.qualcomm.qti.dpm.api@1.0 | \
+            com.qualcomm.qti.imscmservice@2.0 | \
+            com.qualcomm.qti.imscmservice@2.1 | \
+            com.qualcomm.qti.imscmservice@2.2 | \
+            com.qualcomm.qti.uceservice@2.0 | \
+            com.qualcomm.qti.uceservice@2.1 | \
+            com.qualcomm.qti.uceservice@2.2 | \
+            com.qualcomm.qti.uceservice@2.3 | \
+            libmmosal | \
+            vendor.qti.data.factory@2.0 | \
+            vendor.qti.data.factory@2.1 | \
+            vendor.qti.data.factory@2.2 | \
+            vendor.qti.data.factory@2.3 | \
+            vendor.qti.data.mwqem@1.0 | \
+            vendor.qti.data.slm@1.0 | \
+            vendor.qti.diaghal@1.0 | \
+            vendor.qti.hardware.wifidisplaysession@1.0 | \
+            vendor.qti.imsrtpservice@3.0)
+            echo "$1_vendor"
+            ;;
+        libOmxCore | \
+            libwpa_client)
+            # Android.mk only packages
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
