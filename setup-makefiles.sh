24ffede40bc9baaf192bee08077dad6150b4d4dc
Omkar Chandorkar <gotenksIN@aospa.co>
2024-10-10 14:56:30 +0900

    phone1: Fix ELF check failures
    Change-Id: Ie11b68f1cdcd253162029655a5500112cb2e272e
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,6 +24,59 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+        "hardware/qcom/display",
+        "hardware/qcom/display/gralloc",
+        "hardware/qcom/display/libdebug",
+        "vendor/qcom/common/vendor/adreno-r",
+        "vendor/qcom/common/vendor/display/5.4",
+        "vendor/qcom/common/vendor/gps-legacy",
+        "vendor/qcom/common/vendor/media-5.4",
+        "vendor/qcom/common/vendor/wlan-legacy",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.qualcomm.qti.dpm.api@1.0 | \
+            com.qualcomm.qti.imscmservice* | \
+            com.qualcomm.qti.uceservice* | \
+            libmmosal | \
+            vendor.qti.data.* | \
+            vendor.qti.diaghal@1.0 | \
+            vendor.qti.hardware.data.* | \
+            vendor.qti.hardware.embmssl* | \
+            vendor.qti.hardware.limits@1.0 | \
+            vendor.qti.hardware.mwqemadapter@1.0 | \
+            vendor.qti.hardware.radio.* | \
+            vendor.qti.hardware.slmadapter@1.0 | \
+            vendor.qti.hardware.wifidisplaysession@1.0 | \
+            vendor.qti.imsrtpservice@3.0 | \
+            vendor.qti.ims.* | \
+            vendor.qti.latency*)
+            echo "$1_vendor"
+            ;;
+        libOmxCore | \
+            libwpa_client)
+            # Android.mk only packages
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
