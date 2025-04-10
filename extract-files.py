7db00164029430c6a541dc6765338724acb08343
Jyotiraditya Panda <jyotiraditya@aospa.co>
2025-04-10 14:22:18 +0530

    phone1: Switch to python extract-utils
    Change-Id: I59972b098b55ce703e3878bf88f5a8e498fe88c1
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

---
+++ extract-files.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'hardware/qcom/display',
+    'hardware/qcom/display/gralloc',
+    'vendor/qcom/common/vendor/adreno-r',
+    'vendor/qcom/common/vendor/display/5.4',
+    'vendor/qcom/common/vendor/gps-legacy',
+    'vendor/qcom/common/vendor/media-5.4',
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'com.qualcomm.qti.imscmservice@2.0',
+        'com.qualcomm.qti.imscmservice@2.1',
+        'com.qualcomm.qti.imscmservice@2.2',
+        'com.qualcomm.qti.uceservice@2.0',
+        'com.qualcomm.qti.uceservice@2.1',
+        'com.qualcomm.qti.uceservice@2.2',
+        'com.qualcomm.qti.uceservice@2.3',
+        'libmmosal',
+        'vendor.qti.data.factory@2.0',
+        'vendor.qti.data.factory@2.1',
+        'vendor.qti.data.factory@2.2',
+        'vendor.qti.data.factory@2.3',
+        'vendor.qti.data.mwqem@1.0',
+        'vendor.qti.data.slm@1.0',
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.data.cne.internal.api@1.0',
+        'vendor.qti.hardware.data.cne.internal.constants@1.0',
+        'vendor.qti.hardware.data.cne.internal.server@1.0',
+        'vendor.qti.hardware.data.connection@1.0',
+        'vendor.qti.hardware.data.connection@1.1',
+        'vendor.qti.hardware.data.dynamicdds@1.0',
+        'vendor.qti.hardware.data.iwlan@1.0',
+        'vendor.qti.hardware.data.latency@1.0',
+        'vendor.qti.hardware.data.lce@1.0',
+        'vendor.qti.hardware.data.qmi@1.0',
+        'vendor.qti.hardware.embmssl@1.0',
+        'vendor.qti.hardware.embmssl@1.1',
+        'vendor.qti.hardware.limits@1.0',
+        'vendor.qti.hardware.mwqemadapter@1.0',
+        'vendor.qti.hardware.radio.am@1.0',
+        'vendor.qti.hardware.radio.atcmdfwd@1.0',
+        'vendor.qti.hardware.radio.ims@1.0',
+        'vendor.qti.hardware.radio.ims@1.1',
+        'vendor.qti.hardware.radio.ims@1.2',
+        'vendor.qti.hardware.radio.ims@1.3',
+        'vendor.qti.hardware.radio.ims@1.4',
+        'vendor.qti.hardware.radio.ims@1.5',
+        'vendor.qti.hardware.radio.ims@1.6',
+        'vendor.qti.hardware.radio.ims@1.7',
+        'vendor.qti.hardware.radio.ims@1.8',
+        'vendor.qti.hardware.radio.ims@1.9',
+        'vendor.qti.hardware.radio.internal.deviceinfo@1.0',
+        'vendor.qti.hardware.radio.lpa@1.0',
+        'vendor.qti.hardware.radio.lpa@1.1',
+        'vendor.qti.hardware.radio.lpa@1.2',
+        'vendor.qti.hardware.radio.qcrilhook@1.0',
+        'vendor.qti.hardware.radio.qtiradio@1.0',
+        'vendor.qti.hardware.radio.qtiradio@2.0',
+        'vendor.qti.hardware.radio.qtiradio@2.1',
+        'vendor.qti.hardware.radio.qtiradio@2.2',
+        'vendor.qti.hardware.radio.qtiradio@2.3',
+        'vendor.qti.hardware.radio.qtiradio@2.4',
+        'vendor.qti.hardware.radio.qtiradio@2.5',
+        'vendor.qti.hardware.radio.qtiradio@2.6',
+        'vendor.qti.hardware.radio.qtiradio@2.7',
+        'vendor.qti.hardware.radio.uim@1.0',
+        'vendor.qti.hardware.radio.uim@1.1',
+        'vendor.qti.hardware.radio.uim@1.2',
+        'vendor.qti.hardware.radio.uim_remote_client@1.0',
+        'vendor.qti.hardware.radio.uim_remote_client@1.1',
+        'vendor.qti.hardware.radio.uim_remote_client@1.2',
+        'vendor.qti.hardware.radio.uim_remote_server@1.0',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.ims.callcapability@1.0',
+        'vendor.qti.ims.callinfo@1.0',
+        'vendor.qti.ims.configservice@1.0',
+        'vendor.qti.ims.configservice@1.1',
+        'vendor.qti.ims.connection@1.0',
+        'vendor.qti.ims.factory@1.0',
+        'vendor.qti.ims.factory@1.1',
+        'vendor.qti.ims.factory@2.0',
+        'vendor.qti.ims.factory@2.1',
+        'vendor.qti.ims.factory@2.2',
+        'vendor.qti.ims.rcsconfig@1.0',
+        'vendor.qti.ims.rcsconfig@1.1',
+        'vendor.qti.ims.rcsconfig@2.0',
+        'vendor.qti.ims.rcsconfig@2.1',
+        'vendor.qti.ims.rcssip@1.0',
+        'vendor.qti.ims.rcssip@1.1',
+        'vendor.qti.ims.rcssip@1.2',
+        'vendor.qti.ims.rcsuce@1.0',
+        'vendor.qti.ims.rcsuce@1.1',
+        'vendor.qti.ims.rcsuce@1.2',
+        'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.latency@2.0',
+        'vendor.qti.latency@2.1',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libOmxCore',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/bin/hw/vendor.qti.hardware.vibrator.service', 'vendor/lib64/vendor.qti.hardware.vibrator.impl.so'): blob_fixup()
+        .replace_needed('android.hardware.vibrator-V1-ndk_platform.so', 'android.hardware.vibrator-V2-ndk.so'),
+    ('vendor/etc/media_codecs.xml', 'vendor/etc/media_codecs_yupik_v0.xml', 'vendor/etc/media_codecs_yupik_v1.xml'): blob_fixup()
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).*\n', ''),
+    'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
+        .add_line_if_missing('gettid: 1'),
+    ('vendor/lib64/libgf_hal.so'): blob_fixup()
+        .sig_replace('72 6F 2E 62 6F 6F 74 2E 66 6C 61 73 68 2E 6C 6F 63 6B 65 64', '72 6F 2E 62 6F 6F 74 6C 6F 61 64 65 72 2E 6C 6F 63 6B 65 64'),
+    ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'phone1',
+    'nothing',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
