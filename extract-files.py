ca37e05f45a300c078a555a418e65f0f721eaa6e
Jyotiraditya Panda <jyotiraditya@aospa.co>
2026-08-12 17:09:57 +0000

    phone1: Fix adreno namespace path
    Change-Id: I7baf8d76bcfb27b099c291a4c6c31c244c5ed0db

--- extract-files.py
+++ extract-files.py
@@ -15,7 +15,7 @@ from extract_utils.main import ExtractUtils, ExtractUtilsModule
 namespace_imports = [
     'hardware/qcom/display',
     'hardware/qcom/display/gralloc',
-    'vendor/qcom/common/vendor/adreno-r',
+    'vendor/qcom/common/vendor/adreno/r',
     'vendor/qcom/common/vendor/display/5.4',
     'vendor/qcom/common/vendor/gps-legacy',
     'vendor/qcom/common/vendor/media/5.4',
